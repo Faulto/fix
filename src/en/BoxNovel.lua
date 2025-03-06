b74ad6e172c7c7d0555b2b82d41ba0f2d9f08e08
Faulto <ytfaulty@gmail.com>
2025-03-07 00:51:12 +1100

    fix

---
+++ src/en/BoxNovel.lua
@@ -0,0 +1,43 @@
+-- {"id":2,"ver":"3.2.7","libVer":"1.0.0","author":"Doomsdayrs","dep":["Custom>=3.1.5","dkjson"]}
+
+
+
+return Require("Custom")("https://novgo.co", {
+	id = 2,
+	name = "BoxNovel",
+	imageURL = "https://github.com/shosetsuorg/extensions/raw/dev/icons/BoxNovel.png",
+
+	genres = {
+		"Action",
+		"Adventure",
+		"Comedy",
+		"Drama",
+		"Ecchi",
+		"Fantasy",
+		"Gender Bender",
+		"Harem",
+		"Historical",
+		"Horror",
+		"Josei",
+		"Martial Arts",
+		"Mature",
+		"Mecha",
+		"Mystery",
+		"Psychological",
+		"Romance",
+		"School Life",
+		"Sci-fi",
+		"Seinen",
+		"Shoujo",
+		"Shounen",
+		"Slice of Life",
+		"Smut",
+		"Sports",
+		"Supernatural",
+		"Tragedy",
+		"Wuxia",
+		"Xianxia",
+		"Xuanhuan",
+		"Yaoi"
+	}
+})
